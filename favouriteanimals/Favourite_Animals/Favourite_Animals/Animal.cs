2de2b330a9716b51e17417d7bc4489a4da3495e5
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-10-06 12:02:22 +0200

    Favourite animals done

---
+++ favouriteanimals/Favourite_Animals/Favourite_Animals/Animal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace Favourite_Animals
+{
+    class Animal
+    {
+        string path = @"./favourites.txt";
+
+        public void PrintUsage()
+        {
+            Console.WriteLine("```C# FavouriteAnimals [animal] [animal]```");
+        }
+
+        public void AddAnimal(string newAnimal)
+        {
+            string[] content;
+            content = File.ReadAllLines(path);
+
+            foreach (string line in content)
+            {
+                if (newAnimal == line)
+                {
+                    Console.WriteLine("Sorry, but there is another " + newAnimal + " in the file.");
+                    break;
+                }
+                else
+                {
+                    try
+                    {
+                        using (var writer = File.AppendText(path))
+                        {
+                            writer.WriteLine(newAnimal);
+                            Console.WriteLine("Now " + newAnimal + " in the list");
+                            break;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Unable to write file: favourites.txt");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ favouriteanimals/Favourite_Animals/Favourite_Animals/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Favourite_Animals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+
+            if (args.Length == 0)
+            {
+                animal.PrintUsage();
+            }
+
+            if (args.Length == 1)
+            {
+                animal.AddAnimal(args[0]);
+            }
+        }
+    }
+}
